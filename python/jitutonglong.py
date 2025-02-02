c4dd8602155bc8012780e53829089f61d783c00b
hzlyb0813 <hzlyb0813@163.com>
2025-02-02 12:40:30 +0800

    jitutonglong.py

--- python/jitutonglong.py
+++ python/jitutonglong.py
@@ -1,10 +1,12 @@
-t = int(input('头数？'))
-j = int(input('脚数？'))
-if j%2>0:
-    print('你输错了') 
-ji = 2*t-j//2
-tu = t-ji    
-if ji<0 or tu<0:
+import sys
+head = int(input('head？'))
+foot = int(input('foot？'))
+if foot%2>0:
+    print('你输错了')
+    sys.exit()
+chicken = 2*head-foot//2
+rabbit = head-chicken    
+if chicken<0 or rabbit<0:
     print('你输错了') 
 else:    
-    print('鸡有',ji,'只。兔有',tu,'只。')   
+    print('鸡有',chicken,'只。兔有',rabbit,'只。')   
